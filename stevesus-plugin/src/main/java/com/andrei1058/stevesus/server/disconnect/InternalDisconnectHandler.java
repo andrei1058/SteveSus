dbf94936b5b4c21ffdd5ab9b612b38f31a4085e6
andrei1058 <andrew.dascalu@gmail.com>
2020-11-30 13:48:11 +0100

    - fixed disconnect handler

--- stevesus-plugin/src/main/java/com/andrei1058/stevesus/server/disconnect/InternalDisconnectHandler.java
+++ stevesus-plugin/src/main/java/com/andrei1058/stevesus/server/disconnect/InternalDisconnectHandler.java
@@ -22,19 +22,19 @@ public class InternalDisconnectHandler implements DisconnectHandler {
     public void performDisconnect(Player player) {
         TaskChain<?> chain = SteveSus.newChain();
 
-        chain.async(() -> {
-            String[] servers = ServerManager.getINSTANCE().getConfig().getProperty(MainConfig.DISCONNECT_ADAPTER_INTERNAL).trim().split(",");
-            Arrays.asList(servers).forEach(sv -> chain.sync(() -> {
-                if (!player.isOnline()) {
-                    chain.abortChain();
-                }
-                //noinspection UnstableApiUsage
-                ByteArrayDataOutput out = ByteStreams.newDataOutput();
-                out.writeUTF("Connect");
-                out.writeUTF(sv);
-                player.sendPluginMessage(SteveSus.getInstance(), "BungeeCord", out.toByteArray());
-            }).delay(20));
-        }).sync(() -> {
+        String[] servers = ServerManager.getINSTANCE().getConfig().getProperty(MainConfig.DISCONNECT_ADAPTER_INTERNAL).trim().split(",");
+        Arrays.asList(servers).forEach(sv -> chain.sync(() -> {
+            if (!player.isOnline()) {
+                chain.abortChain();
+            }
+            //noinspection UnstableApiUsage
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF(sv);
+            player.sendPluginMessage(SteveSus.getInstance(), "BungeeCord", out.toByteArray());
+        }).delay(20));
+
+        chain.sync(() -> {
             if (player.isOnline()) {
                 player.kickPlayer(ChatColor.BLUE + "You're not supposed to be still here!\n It looks like fallback lobbies are down and you were kicked.");
             }
