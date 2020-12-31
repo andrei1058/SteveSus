27faeb5da82c6c28353f59e6978b89cc06e4db74
andrei1058 <andrew.dascalu@gmail.com>
2020-12-31 17:21:14 +0100

    fixed /ss closeInv showing up

--- stevesus-common/src/main/java/com/andrei1058/stevesus/common/command/InvCloseCmd.java
+++ stevesus-common/src/main/java/com/andrei1058/stevesus/common/command/InvCloseCmd.java
@@ -3,6 +3,7 @@ package com.andrei1058.stevesus.common.command;
 import com.andrei1058.spigot.commandlib.fast.FastRootCommand;
 import com.andrei1058.spigot.commandlib.fast.FastSubCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 
 @SuppressWarnings("UnstableApiUsage")
 public class InvCloseCmd {
@@ -13,7 +14,7 @@ public class InvCloseCmd {
     public static void register(FastRootCommand root) {
         root
                 .withSubNode(new FastSubCommand("closeInv")
-                .withPermAdditions(s -> (s instanceof Player) && ((Player)s).getOpenInventory() != null && ((Player)s).getOpenInventory().getTopInventory() != null)
+                .withPermAdditions(s -> (s instanceof Player) && (!(((Player)s).getOpenInventory() instanceof PlayerInventory)))
                 .withExecutor((s, args) -> ((Player)s).closeInventory()));
 
     }
